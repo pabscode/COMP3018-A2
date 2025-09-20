db8335f360feca948edf835aaf373725fe08634c
pabscode <psalazar105@rrc.ca>
2025-09-20 18:24:19 -0500

    Routes: Implementing Employee directory endpoints.

---
+++ src/api/v1/routes/employeesRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Router } from "express";
+import * as employeeController from "../controllers/employeeController";
+
+const router: Router = express.Router();
+
+// "/api/v1/items" prefixes all below routes
+router.get("/", employeeController.getAllEmployees);
+router.post("/", employeeController.createEmployee);
+router.put("/:id", employeeController.updateEmployee);
+router.delete("/:id", employeeController.deleteEmployee);
+
+export default router;
