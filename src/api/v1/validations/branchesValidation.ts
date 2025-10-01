dbe2376963a1cb1d432dffa8679f3f31a8ad6ddd
pabscode <psalazar105@rrc.ca>
2025-10-01 10:41:09 -0500

    Validation: Created joi validation for Branches.

---
+++ src/api/v1/validations/branchesValidation.ts
@@ -0,0 +1,45 @@
+import Joi from "joi";
+
+/**
+ * Branches Schema organised by request type
+ */
+export const branchesSchema = {
+    // POST /api/v1/branches - Create New Branch
+    create: {
+        body: Joi.object({
+            name: Joi.string().required().messages({
+                "any.required": "Name is required",
+                "string.empty": "Name cannot be empty",
+            }),
+            address: Joi.string().required().messages({
+                "any.required": "Address is required",
+                "string.empty": "Address cannot be empty",
+            }),
+            phone: Joi.string().required().messages({
+                "any.required": "Phone is required",
+                "string.empty": "Phone cannot be empty",
+            }),
+        }),
+    },
+
+    // PUT /api/v1/branches/:id - Update Branch
+    update: {
+        params: Joi.object({
+            id: Joi.string().required().messages({
+                "any.required": "Branch ID is required",
+                "string.empty": "Branch ID cannot be empty",
+            }),
+        }),
+        body: Joi.object({
+            name: Joi.string().optional().messages({
+                "string.empty": "Name cannot empty",
+            }),
+            address: Joi.string().optional().messages({
+                "string.empty": "Address cannot be empty",
+            }),
+            aphonedress: Joi.string().optional().messages({
+                "string.empty": "Phone cannot be empty",
+            }),
+        }),
+    },
+};
