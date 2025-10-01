42c706bd970022110aeb3bbf5be3cab68af86d81
pabscode <psalazar105@rrc.ca>
2025-10-01 09:40:11 -0500

    Validation Schema: Created the files to implement Joi validation schemas.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
+    testMatch: ["**/*.test.ts"],
     setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
+    moduleNameMapper: {
+        "^src/(.*)$": "<rootDir>/src/$1"
+    },
+    roots: ["<rootDir>/src", "<rootDir>/test"],
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!src/server.ts", // Exclude server startup file
+        "!src/types/**/*.ts", // Exclude type definitions
+    ],
 };
